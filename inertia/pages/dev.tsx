7dc6310ac288748c0417eec0417e6d5db6427685
eviltartiflette <maximerancon@gmail.com>
2026-08-08 15:05:34 +0200

    cleanup unused views

--- inertia/pages/dev.tsx
+++
@@ -1,8 +0,0 @@
-export default function Dev() {
-  return (
-    <section>
-      <h1 className="h2 mb-3">Dev</h1>
-      <p className="mb-0">Software development projects will be listed here.</p>
-    </section>
-  )
-}
--- inertia/pages/multimedia.tsx
+++
@@ -1,8 +0,0 @@
-export default function Multimedia() {
-  return (
-    <section>
-      <h1 className="h2 mb-3">Multimedia</h1>
-      <p className="mb-0">Multimedia and creative projects will be listed here.</p>
-    </section>
-  )
-}
